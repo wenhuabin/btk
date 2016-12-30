574cfa170cb968533f34e57601a4617dbe865aca
温华斌 <wenhuabin@wenhuabindeMacBook-Pro.local>
2016-12-30 15:56:51 +0800

    移动端网页调整

--- server/app/route.js
+++ server/app/route.js
@@ -7,9 +7,10 @@ var path = require('path');
 
 router.get('/', function (req, res, next) {
     if(req.headers['user-agent'].match(/(iPhone|iPod|Android)/i)){
-        //console.log("mobile");
+        console.log("mobile");
         res.sendFile(path.join(__dirname, '../../bin/mindex.html'));
     }else{
+        console.log("pc");
         res.sendFile(path.join(__dirname, '../../bin/index.html'));
     }
 })
